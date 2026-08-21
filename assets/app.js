a41213bcf8d2486bf0a38404aa8f3417ca752ea1
Issa Yaici <issa.growth@gmail.com>
2026-08-21 14:12:00 +0200

    Passe responsive accueil (WIP du 20/08) : carrousel témoignages glissable, mission en ligne, carte livre recadrée

--- assets/app.js
+++ assets/app.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
       entries.forEach(e => {
         if (e.isIntersecting) { e.target.classList.add('in'); io.unobserve(e.target); }
       });
-    }, { threshold: 0.12, rootMargin: '0px 0px -8% 0px' });
+    }, { threshold: 0, rootMargin: '0px 0px -6% 0px' });
     els.forEach(el => io.observe(el));
   } else {
     els.forEach(el => el.classList.add('in'));
